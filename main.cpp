034f07707eaeb96ba1d588cdab5c59fbc37810fd
Shimao1211 <hj-g20010@sist.ac.jp>
2021-05-07 12:28:34 +0900

    円を描画して動かした

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 
 #define GAME_ICON_ID	333				//ゲームのICONのID
 
+#define GAME_WINDOW_BAR 0				//ウィンドウバーの種類
+
 // プログラムは WinMain から始まります
 //windowsのプログラミング方法＝（WinAPI）で動いている！
 //DxLibは、DirectXという、ゲームプログラミングを簡単に使える仕組み
@@ -18,17 +20,38 @@ int WINAPI WinMain(
 	LPSTR lpCmdLine, 
 	int nCmdShow)
 {
-	ChangeWindowMode(TRUE);					//ウィンドウモードに設定
-	SetMainWindowText("ゲームタイトル");	//ウィンドウのタイトルの文字
+	SetOutApplicationLogValidFlag(FALSE);				//log.txtを出力しない
+	ChangeWindowMode(TRUE);								//ウィンドウモードに設定
+	SetMainWindowText("ゲームタイトル");				//ウィンドウのタイトルの文字
 	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);	//ウィンドウの解像度を設定
 	SetWindowSize(GAME_WIDTH, GAME_HEIGHT);				//ウィンドウの大きさを設定
 	SetBackgroundColor(255, 255, 255);					//デフォルトの背景の色
-	SetWindowIconID(GAME_ICON_ID);
+	SetWindowIconID(GAME_ICON_ID);						//アイコンファイルを読み込み
+	SetWindowStyleMode(GAME_WINDOW_BAR);				//ウィンドウバーの状態
+	SetWaitVSyncFlag(TRUE);								//ディスプレイの垂直同期を有効にする
+	SetAlwaysRunFlag(TRUE);								//ウィンドウをずっとアクティブにする
 
 	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
 	{
 		return -1;			// エラーが起きたら直ちに終了
 	}
+
+	//四角の位置を決める
+	int X = GAME_WIDTH / 2;
+	int Y = GAME_HEIGHT / 2;
+
+	//四角の大きさを決める
+	int width = 32;		//幅
+	int height = 32;	//高さ
+
+	int radius = 40;
+
+	//速度
+	int speed = 1;
+	int Xspeed = 1;
+	int Yspeed = 1;
+
+	SetDrawScreen(DX_SCREEN_BACK);
 
 	//無限ループ
 	while (1)
@@ -44,7 +67,49 @@ int WINAPI WinMain(
 		{
 			break;	//無限ループを抜ける
 		}
-	
+
+		//画面を消去する
+		if (ClearDrawScreen() != 0) { break; }
+
+		//四角を描画
+		DrawBox(
+			X, Y, X + width, Y + height,
+			GetColor(255, 0, 0),		//色を取得
+			TRUE						//塗りつぶし？
+		);
+
+		//円を描画
+		DrawCircle(
+			X, Y, radius,
+			GetColor(0, 255, 0),
+				FALSE, 5
+			);
+
+		//四角を右に動かす
+		X += Xspeed;
+		Y += Yspeed;
+
+		if (X < 0 || X + width > GAME_WIDTH)	//画面の横から出たとき
+		{ 
+			//+1なら-1にする　/ -1なら+1にする
+			Xspeed = -Xspeed; //移動する向きを反転
+
+			//壁に当たると速くなる
+			if (Xspeed > 0) { Xspeed += 2; }
+			else if (Xspeed < 0) { Xspeed -= 2; }
+		}
+
+		if (Y < 0 || Y + height > GAME_HEIGHT)	//画面の縦から出たとき
+		{
+			Yspeed = -Yspeed;
+
+			//壁に当たると速くなる
+			if (Yspeed > 0) { Yspeed += 2; }
+			else if (Yspeed < 0) { Yspeed -= 2; }
+		}
+
+
+		ScreenFlip();		//ダブルバッファリングした画面を描画
 	}
 
 	DxLib_End();				// ＤＸライブラリ使用の終了処理
