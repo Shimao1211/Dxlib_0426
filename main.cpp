0e8adcf030ffcc4e8785aa5ccb4793b49983ab99
Shimao1211 <hj-g20010@sist.ac.jp>
2021-05-10 12:29:18 +0900

    キーボード処理のヘッダファイルを作った

--- main.cpp
+++ main.cpp
@@ -36,78 +36,19 @@ int WINAPI WinMain(
 		return -1;			// エラーが起きたら直ちに終了
 	}
 
-	//四角の位置を決める
-	int X = GAME_WIDTH / 2;
-	int Y = GAME_HEIGHT / 2;
-
-	//四角の大きさを決める
-	int width = 32;		//幅
-	int height = 32;	//高さ
-
-	int radius = 40;
-
-	//速度
-	int speed = 1;
-	int Xspeed = 1;
-	int Yspeed = 1;
+	
 
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	//無限ループ
 	while (1)
 	{
-		//何かしらキーが押されたとき
-		if (CheckHitKeyAll() != 0)	//０のときに、何かキーが押された
-		{
-			break;	//無限ループを抜ける
-		}
+		
 
 		//メッセージを受け取り続ける
-		if (ProcessMessage() != 0)	//-1のとき、エラーやウィンドウが閉じられた
-		{
-			break;	//無限ループを抜ける
-		}
-
-		//画面を消去する
+		if (ProcessMessage() != 0) { break; }	//-1のとき、エラーやウィンドウが閉じられた
 		if (ClearDrawScreen() != 0) { break; }
-
-		//四角を描画
-		DrawBox(
-			X, Y, X + width, Y + height,
-			GetColor(255, 0, 0),		//色を取得
-			TRUE						//塗りつぶし？
-		);
-
-		//円を描画
-		DrawCircle(
-			X, Y, radius,
-			GetColor(0, 255, 0),
-				FALSE, 5
-			);
-
-		//四角を右に動かす
-		X += Xspeed;
-		Y += Yspeed;
-
-		if (X < 0 || X + width > GAME_WIDTH)	//画面の横から出たとき
-		{ 
-			//+1なら-1にする　/ -1なら+1にする
-			Xspeed = -Xspeed; //移動する向きを反転
-
-			//壁に当たると速くなる
-			if (Xspeed > 0) { Xspeed += 2; }
-			else if (Xspeed < 0) { Xspeed -= 2; }
-		}
-
-		if (Y < 0 || Y + height > GAME_HEIGHT)	//画面の縦から出たとき
-		{
-			Yspeed = -Yspeed;
-
-			//壁に当たると速くなる
-			if (Yspeed > 0) { Yspeed += 2; }
-			else if (Yspeed < 0) { Yspeed -= 2; }
-		}
-
+		
 
 		ScreenFlip();		//ダブルバッファリングした画面を描画
 	}
